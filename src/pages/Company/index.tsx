b355e93f92d3dc34663c9e17cb9bb3f8911b3cd6
waghnerreis <waghnerreis@gmail.com>
2020-12-14 10:07:12 -0300

    screen structure and styles

--- src/pages/Company/index.tsx
+++ src/pages/Company/index.tsx
@@ -1,7 +1,28 @@
-import React from 'react';
+import React, { useMemo } from 'react';
+import { useParams } from 'react-router-dom';
+import { useCompany } from '../../hooks/company';
 
-// import { Container } from './styles';
+import { Container } from './styles';
 
-const Company: React.FC = () => <h1>Company</h1>;
+interface ParamsData {
+  id: string;
+}
+
+const Company: React.FC = () => {
+  const { getCompany } = useCompany();
+  const { id } = useParams<ParamsData>();
+
+  const company = useMemo(() => getCompany(id), [getCompany, id]);
+
+  return (
+    <Container>
+      <img
+        src={`https://empresas.ioasys.com.br${company?.photo}`}
+        alt={`Logo da ${company?.enterprise_name}`}
+      />
+      <span>{company?.description}</span>
+    </Container>
+  );
+};
 
 export default Company;
